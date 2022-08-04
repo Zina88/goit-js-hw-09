4a77165988e57cce1884558f61db67e6cce72159
Zina <zinasaenko@hotmail.com>
2022-08-04 11:28:50 +0300

    edits

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+
+let intervalId = null;
+let isActive = false;
+
+function onStartClick() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+  isActive = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
