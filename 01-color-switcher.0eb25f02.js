ba80b657336aea24af3e9cefc60a5b6b814f9d6e
Zina88 <97629055+Zina88@users.noreply.github.com>
2022-08-04 10:47:43 +0000

    Deploying to gh-pages from @ Zina88/goit-js-hw-09@339b7168bbc7242401d712093fc4dde5d48d606d 🚀

---
+++ 01-color-switcher.0eb25f02.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};t.start.addEventListener("click",(function(){t.start.disabled=!0,t.stop.disabled=!1,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(function(){t.start.disabled=!1,t.stop.disabled=!0,clearInterval(e)})),t.stop.disabled=!0;let e=null;
+//# sourceMappingURL=01-color-switcher.0eb25f02.js.map
