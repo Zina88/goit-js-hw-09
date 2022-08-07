597fb84f473c1572313951fada1cd3a896b0ee34
Zina <zinasaenko@hotmail.com>
2022-08-07 08:40:21 +0300

    :star:

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,6 +13,10 @@ function onSubmitForm(e) {
   const step = Number(e.currentTarget.step.value);
   const amount = Number(e.currentTarget.amount.value);
 
+  console.log('delay', delay);
+  console.log('step', step);
+  console.log('amount', amount);
+
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
@@ -44,7 +48,3 @@ function createPromise(position, delay) {
     reject(objectPromise);
   });
 }
-
-// Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз, сколько ввели в поле amount. При каждом вызове передай ей номер создаваемого промиса (position) и задержку учитывая введенную пользователем первую задержку (delay) и шаг (step).
-
-// Дополни код функции createPromise так, чтобы она возвращала один промис, который выполянется или отклоняется через delay времени. Значением промиса должен быть объект, в котором будут свойства position и delay со значениями одноименных параметров. Используй начальный код функции для выбора того, что нужно сделать с промисом - выполнить или отклонить.
